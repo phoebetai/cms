c9d505e89b86edab5826b2b94b93005d9f6bd0d7
Phoebe <kptai@terpmail.umd.edu>
2018-02-12 22:18:35 -0500

    file i/o test

--- main.cpp
+++ main.cpp
@@ -1,18 +1,14 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main() {
 
-  int i = 10;
-  cout << "The memory address of i is " << &i << "\n";
-  cout << "The data stored at memory address " << &i << " is " << i << "\n";
-
-  int *p = &i;
-  cout << "The value of p is " << p << "\n";
-  cout << "We say that p 'points at' the memory location referenced by the address " << p << "\n";
-
-  cout << "The data stored at memory address " << p << " is " << *p << "\n";
+  ofstream myfile;
+  myfile.open("example.txt");
+  myfile << "helllooOOOOooo";
+  myfile.close();
 
   return 0;
 
