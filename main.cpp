448755bc97d8ce7c5e2152976d1898aa1144312c
Phoebe <kptai@terpmail.umd.edu>
2018-02-12 20:27:30 -0500

    testing operations

--- main.cpp
+++ main.cpp
@@ -4,5 +4,15 @@ using namespace std;
 
 int main() {
   cout << "Hello world!" << endl;
+  
+  int i = 2;
+  cout << "i = " << i << endl;
+  
+  double a = 3.3;
+  cout << "a = " << a << endl;
+
+  int j = a * i;
+  cout << "a * i = " << j << endl;
+
   return 0;
 }
